f7fa01d49d2c1b765cd1b98757606b6d7df27d11
Grzegorz Dorożko <dorozko.grzegorz@gmail.com>
2020-06-01 18:04:57 +0200

    strona 2

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,18 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Nie umiem w react. Umiem w Xamarin Android.
+        </p>
+        <p>
+          Test akapitu.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="pagetwo.tsx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Strona 2
         </a>
       </header>
     </div>
